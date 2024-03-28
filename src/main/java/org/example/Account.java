ce4f431af7eec6904435adf04d1fd8801a118603
Hexia <you@example.com>
2024-03-28 14:08:43 +0100

    Initial commit

---
+++ src/main/java/org/example/Account.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class Account {
+    String owner;
+    double balance;
+    double minimumBalance;
+    public void deposit(double amount) {
+
+    }
+
+    public void withdraw(double amount){
+
+    }
+}
